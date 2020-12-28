d630054df68251e5604bd430cd0ca39fec38fb6d
arongerson <gerson.aron@yahoo.com>
2020-12-28 13:09:39 -0500

    handle error

--- arithmetic/arithmetic.go
+++ arithmetic/arithmetic.go
@@ -1,12 +1,17 @@
 package arithmetic
 
+import "errors"
+
 // Add adds two numbers
-func Add(nums ...int) int {
+func Add(nums ...int) (int, error) {
+	if len(nums) < 2 {
+		return 0, errors.New("A minimum of 2 numbers is required")
+	}
 	sum := 0
 	for _, val := range nums {
 		sum += val
 	}
-	return sum
+	return sum, nil
 }
 
 // Subtract subtracts the second operand from the first operand
