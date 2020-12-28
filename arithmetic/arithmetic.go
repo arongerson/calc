bfc971a959d7f0c0afb2539e8811fc96b388a502
arongerson <gerson.aron@yahoo.com>
2020-12-28 09:29:41 -0500

    added comments to exported methods

--- arithmetic/arithmetic.go
+++ arithmetic/arithmetic.go
@@ -1,9 +1,11 @@
 package arithmetic
 
+// Add adds two numbers
 func Add(num1 int, num2 int) int {
 	return num1 + num2
 }
 
+// Subtract subtracts the second operand from the first operand
 func Subtract(num1 int, num2 int) int {
 	return num1 - num2
 }
