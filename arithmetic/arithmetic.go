b857838f7b750ad9a2c8c192537f54cc8e516fc4
arongerson <gerson.aron@yahoo.com>
2020-12-28 09:27:46 -0500

    export methods in package arithmetic

--- arithmetic/arithmetic.go
+++ arithmetic/arithmetic.go
@@ -1,9 +1,9 @@
 package arithmetic
 
-func add(num1 int, num2 int) int {
+func Add(num1 int, num2 int) int {
 	return num1 + num2
 }
 
-func subtract(num1 int, num2 int) int {
+func Subtract(num1 int, num2 int) int {
 	return num1 - num2
 }
