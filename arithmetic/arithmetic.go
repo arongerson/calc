66e2306da6bda1c04dea59cd82d3d82d01875d1c
arongerson <gerson.aron@yahoo.com>
2020-12-28 11:21:09 -0500

    use variadic function for Add

--- arithmetic/arithmetic.go
+++ arithmetic/arithmetic.go
@@ -1,8 +1,12 @@
 package arithmetic
 
 // Add adds two numbers
-func Add(num1 int, num2 int) int {
-	return num1 + num2
+func Add(nums ...int) int {
+	sum := 0
+	for _, val := range nums {
+		sum += val
+	}
+	return sum
 }
 
 // Subtract subtracts the second operand from the first operand
